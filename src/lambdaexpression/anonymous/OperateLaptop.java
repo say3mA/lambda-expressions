f6b5bc884843f45539ceaa3a2f37c68550ec7223
say3mA <smsd577@gmail.com>
2020-08-16 14:50:41 -0400

    initial commit

---
+++ src/lambdaexpression/anonymous/OperateLaptop.java
@@ -0,0 +1,19 @@
+package lambdaexpression.anonymous;
+
+public class OperateLaptop {
+
+    public static void main(String[] args) {
+        //Please implement anonymous class for interface laptop
+        Laptop laptop = null;
+
+
+
+
+        laptop.monitorSize(16);
+        laptop.monitorSize(15);
+
+        //please implement anonymous function for Phone FunctionalInterface
+
+
+    }
+}
