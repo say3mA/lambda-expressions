669868f680d966bbeb85796c4cf189889d0a5fd1
say3mA <smsd577@gmail.com>
2020-08-18 13:25:22 -0400

    Update OperateLaptop.java

--- src/lambdaexpression/anonymous/OperateLaptop.java
+++ src/lambdaexpression/anonymous/OperateLaptop.java
@@ -4,16 +4,28 @@ public class OperateLaptop {
 
     public static void main(String[] args) {
         //Please implement anonymous class for interface laptop
-        Laptop laptop = null;
+        Laptop laptop = new Laptop() {
+            @Override
+            public void ramSize(int memorySize) {
+                System.out.println("Laptop ram size is " + memorySize);
+            }
 
-
-
+            @Override
+            public void monitorSize(int screenSize){
+                System.out.println("Monitor size is "+ screenSize);
+            }
+        };
 
         laptop.monitorSize(16);
         laptop.monitorSize(15);
 
+        Phone phone = new Phone(){
+            @Override
+            public void cellularNetwork(String provider){
+                System.out.println("cell provider is " + provider);
+            }
+        };
+        phone.cellularNetwork("Verizon");
         //please implement anonymous function for Phone FunctionalInterface
-
-
     }
 }
